139d201afe2a69d3519ac4bfcc0f893ff2a7393b
nmtiens <nguyenminhtien09012003@gmail.com>
2024-10-03 08:07:06 +0700

    Cai dat cac handler truy xuat csdl

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -12,9 +12,9 @@ router.route("/")
 router.route("/favorite")
     .get(contacts.findAllFavorite);
 
-router.route("/:id")
-    .get(contacts.findOne)
-    .put(contacts.update)
-    .delete(contacts.delete);
+    router.route("/:id")
+        .get(contacts.findOne)
+        .put(contacts.update)
+        .delete(contacts.delete);
 
 module.exports = router;
